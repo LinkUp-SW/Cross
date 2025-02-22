4c596053ebe11651df540520aeb8a5132f95776d
Youssef Hassanien <Youssefhassanein17@gmail.com>
2025-02-22 13:48:56 +0200

    Buttons styles added

--- lib/shared/themes/button_styles.dart
+++ lib/shared/themes/button_styles.dart
@@ -1,1 +1,65 @@
-// The different buttons in different pages
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:link_up/shared/themes/colors.dart';
+import 'package:link_up/shared/themes/text_styles.dart';
+
+class LinkUpButtonStyles {
+  ButtonStyle profileOpenToLight({
+    double elevation = 0.0,
+    EdgeInsets? padding,
+    BorderRadius? borderRadius,
+    TextStyle? textStyle,
+  }) {
+    padding ??= EdgeInsets.symmetric(horizontal: 10.w, vertical: 8.h);
+    borderRadius ??= BorderRadius.all(Radius.circular(20.r));
+    textStyle ??= TextStyles.font13_700Weight;
+
+    return ButtonStyle(
+      foregroundColor: WidgetStateProperty.all(AppColors.lightMain),
+      backgroundColor: WidgetStateProperty.all(AppColors.lightBlue),
+      elevation: WidgetStateProperty.all(elevation),
+      padding: WidgetStateProperty.all(padding),
+      shape: WidgetStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: borderRadius,
+          side: BorderSide(
+            color: AppColors.lightBlue,
+            width: 1.w,
+          ),
+        ),
+      ),
+      textStyle: WidgetStateProperty.all(textStyle),
+    );
+  }
+
+  ButtonStyle profileOpenToDark({
+    double elevation = 0.0,
+    EdgeInsets? padding,
+    BorderRadius? borderRadius,
+    TextStyle? textStyle,
+  }) {
+    padding ??= EdgeInsets.symmetric(horizontal: 10.w, vertical: 8.h);
+    borderRadius ??= BorderRadius.all(Radius.circular(20.r));
+    textStyle ??= TextStyles.font13_700Weight;
+
+    return ButtonStyle(
+      foregroundColor: WidgetStateProperty.all(AppColors.darkMain),
+      backgroundColor: WidgetStateProperty.all(AppColors.darkBlue),
+      elevation: WidgetStateProperty.all(elevation),
+      padding: WidgetStateProperty.all(padding),
+      shape: WidgetStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: borderRadius,
+          side: BorderSide(
+            color: AppColors.lightBlue,
+            width: 1.w,
+          ),
+        ),
+      ),
+      textStyle: WidgetStateProperty.all(textStyle),
+    );
+  }
+}
+
+
+// The rest of buttons styles should be exist here
