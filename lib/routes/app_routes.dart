dfce466ded6dededbef269008b126bc6c97ce93c
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-15 01:24:10 +0200

    Modification: Added signup to the list of accessible routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,15 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:link_up/features/logIn/view/view.dart';
+import 'package:link_up/features/signUp/view/signingup_view.dart';
 import 'package:link_up/shared/dummy_page.dart';
 import 'package:link_up/shared/myhomepage.dart';
 import 'package:link_up/shared/widgets/bottom_navigation_bar.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(initialLocation: '/login', routes: <RouteBase>[
+  return GoRouter(initialLocation: '/signup', routes: <RouteBase>[
     GoRoute(path: "/profile", builder: (context, state) => Container()),
     GoRoute(path: "/login", builder: (context, state) => const LoginPage()),
-    GoRoute(path: "/signup", builder: (context, state) => Container()),
+    GoRoute(path: "/signup", builder: (context, state) => const SigningupView()),
     StatefulShellRoute.indexedStack(
       builder: (context, state, navigationShell) => Scaffold(
         body: navigationShell, // The body displays the current screen
