4f049c306e76f9d2d1ff0cecfd84915d8ec8572e
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-16 13:35:35 +0200

    Modification: Added Providers form Getting names and getting phone numbers

--- lib/features/signUp/viewModel/view_model.dart
+++ lib/features/signUp/viewModel/view_model.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//////////////////////////// SigningUpViewModel  ////////////////////////////
 class SigningUpViewModel extends ChangeNotifier {
   String? validateEmail(String? value) {
     if (value == null ||
@@ -21,5 +22,45 @@ class SigningUpViewModel extends ChangeNotifier {
   }
 }
 
+//////////////////////////// NamingViewModel  ////////////////////////////
 final signingUpViewModelprovider =
     ChangeNotifierProvider((ref) => SigningUpViewModel());
+
+class NamingViewModel extends ChangeNotifier {
+  String? validateName(String? value, String? error) {
+    if (value == null || value.isEmpty || value.length < 3) {
+      return 'Please Insert a valid $error';
+    }
+    return null;
+  }
+}
+
+final namingViewModelProvider =
+    ChangeNotifierProvider((ref) => NamingViewModel());
+
+
+
+
+
+
+
+
+//////////////////////////// GetPhoneNumberViewModel  ////////////////////////////
+class GetPhoneNumberViewModel extends ChangeNotifier {
+  String phoneNumber = '';
+  
+  void setPhoneNumber(String value) {
+    phoneNumber = value;
+    notifyListeners();
+  }
+
+  String? validatePhoneNumber(String? value) {
+    if (value == null || value.isEmpty || value.length < 10) {
+      return 'Please enter a valid phone number';
+    }
+    return null;
+  }
+}
+
+final getPhoneNumberViewModelProvider =
+    ChangeNotifierProvider((ref) => GetPhoneNumberViewModel());
