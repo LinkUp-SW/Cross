6a9be5e2174dc113fca57ee63df711a8e02f2015
Youssef Hassanien <youssefhassanien17@gmail.com>
2025-03-18 22:19:53 +0200

    Received and Sent tabs

--- lib/features/my-network/view/invitations.dart
+++ lib/features/my-network/view/invitations.dart
@@ -14,34 +14,68 @@ class InvitationsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(
-          'Invitations',
-          style: TextStyles.font20_700Weight.copyWith(
-            color: isDarkMode ? AppColors.darkGrey : AppColors.lightTextColor,
-          ),
-        ),
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            size: 25.w,
-            color: isDarkMode
-                ? AppColors.darkSecondaryText
-                : AppColors.lightSecondaryText,
-          ),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.settings,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(
+            'Invitations',
+            style: TextStyles.font20_700Weight.copyWith(
               color: isDarkMode ? AppColors.darkGrey : AppColors.lightTextColor,
             ),
-          )
-        ],
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(kToolbarHeight),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: isDarkMode
+                    ? AppColors.darkBackground
+                    : AppColors.lightBackground,
+              ),
+              child: TabBar(
+                isScrollable: true,
+                tabAlignment: TabAlignment.start,
+                labelPadding: EdgeInsets.symmetric(horizontal: 15.w),
+                labelStyle: TextStyles.font13_500Weight,
+                unselectedLabelStyle: TextStyles.font13_500Weight,
+                labelColor:
+                    isDarkMode ? AppColors.darkGreen : AppColors.lightGreen,
+                unselectedLabelColor: isDarkMode
+                    ? AppColors.darkSecondaryText
+                    : AppColors.lightSecondaryText,
+                tabs: const [
+                  Tab(
+                    text: "Received",
+                  ),
+                  Tab(
+                    text: "Sent",
+                  ),
+                ],
+              ),
+            ),
+          ),
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              size: 25.w,
+              color: isDarkMode
+                  ? AppColors.darkSecondaryText
+                  : AppColors.lightSecondaryText,
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color:
+                    isDarkMode ? AppColors.darkGrey : AppColors.lightTextColor,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
