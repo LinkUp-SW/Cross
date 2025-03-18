ccd6c8cd15063ae35faf017423de71c2b4dd8ce5
A-Aref <abdallahahmadaref@gmail.com>
2025-03-18 09:28:25 +0200

    Made the page use the initial() to create its dummy data

--- lib/features/Home/view/reposts_page.dart
+++ lib/features/Home/view/reposts_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:link_up/features/Home/home_enums.dart';
-import 'package:link_up/features/Home/model/header_model.dart';
 import 'package:link_up/features/Home/model/post_model.dart';
 import 'package:link_up/features/Home/widgets/posts.dart';
 import 'package:link_up/shared/themes/colors.dart';
@@ -16,31 +14,13 @@ class RepostsPage extends StatefulWidget {
 }
 
 class _RepostsPageState extends State<RepostsPage> {
-  List<PostModel> posts = List.generate(10, (index) => PostModel(
-    id: '1',
-    header: HeaderModel(
-      profileImage: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-      name: 'John Doe',
-      connectionDegree: 'johndoe',
-      about: 'About John Doe',
-      timeAgo: DateTime.now(),
-      visibility: Visibilities.anyone,
-    ),
-    text: 'This is a post',
-    media: Media(
-      type: MediaType.image,
-      urls: ['https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'],
-    ),
-    reactions: 10,
-    comments: 50,
-    reposts: 0,
-    reaction: Reaction.celebrate,
-  ));
+  List<PostModel> posts = List.generate(10, (index) => PostModel.initial());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Reposts'),
         backgroundColor: Theme.of(context).colorScheme.primary,
         leading: IconButton(
