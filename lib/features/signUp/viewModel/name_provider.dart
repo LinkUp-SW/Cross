41983d7add2492206c6c677410c2685126bfaa0a
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-24 02:14:44 +0200

    Modifications: Removed redundant data

--- lib/features/signUp/viewModel/name_provider.dart
+++ lib/features/signUp/viewModel/name_provider.dart
@@ -7,36 +7,18 @@ import 'package:link_up/features/signUp/viewModel/signup_notifier.dart';
 final nameServiceProvider = Provider((ref) => NameService());
 
 final nameProvider = StateNotifierProvider<NameNotifier, NameState>((ref) {
-  final service = ref.watch(nameServiceProvider);
   final signUpNotifier =
       ref.read(signUpProvider.notifier); // Access the global signup instance
-  return NameNotifier(service, signUpNotifier);
+  return NameNotifier( signUpNotifier);
 });
 
 class NameNotifier extends StateNotifier<NameState> {
-  final NameService _nameService;
   final SignUpNotifier _signUpNotifier;
 
-  NameNotifier(this._nameService, this._signUpNotifier) : super(NameInitial());
+  NameNotifier( this._signUpNotifier) : super(NameInitial());
 
-  Future<void> setName(String firstName, String lastName) async {
-    try {
-      state = NameLoading();
-      final success = await _nameService
-          .setName(NameModel(firstName: firstName, lastName: lastName));
-      if (success == true) {
-        // Update the global signup instance
-        _signUpNotifier.updateUserData(
-            firstName: firstName, lastName: lastName);
-        print(
-            "First Name: ${_signUpNotifier.state.fisrtName}, Last Name: ${_signUpNotifier.state.lastName}");
-        state = NameValid();
-      } else {
-        state = NameInvalid("Failed to set name");
-      }
-    } catch (e) {
-      state = NameInvalid("Failed to set name");
-    }
+  void setName(String firstName, String lastName) {
+    _signUpNotifier.updateUserData(firstName: firstName, lastName: lastName);
   }
 
   String? validateName(String? name, String? definer) {
