9e08e7420b9601b64aba3ec81215ded98b38614c
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-24 02:03:50 +0200

    modification: added more tags for sign up

--- lib/features/signUp/model/signup_global_model.dart
+++ lib/features/signUp/model/signup_global_model.dart
@@ -1,19 +1,35 @@
 class SignUpModel {
-  String? fisrtName;
+  String? firstName;
   String? lastName;
   String? email;
   String? password;
   String? phoneNumber;
+  String? countryCode;
+  String? country;
+  String? city;
+  String? jobTitle;
+  String? companyName;
+  String? school;
+  String? startDate;
+  String? location;
   String? address;
   String? dateOfBirth;
   String? gender;
 
   SignUpModel({
-    this.fisrtName,
+    this.firstName,
     this.lastName,
     this.email,
     this.password,
     this.phoneNumber,
+    this.countryCode,
+    this.country,
+    this.city,
+    this.jobTitle,
+    this.companyName,
+    this.school,
+    this.startDate,
+    this.location,
     this.address,
     this.dateOfBirth,
     this.gender,
@@ -22,14 +38,41 @@ class SignUpModel {
   // Convert to JSON for API submission
   Map<String, dynamic> toJson() {
     return {
-      "firstName": fisrtName,
+      "firstName": firstName,
       "lastName": lastName,
       "email": email,
       "password": password,
       "phoneNumber": phoneNumber,
+      "countryCode": countryCode,
+      "country": country,
+      "city": city,
+      "jobTitle": jobTitle,
+      "companyName": companyName,
+      "school": school,
+      "startDate": startDate,
+      "location": location,
       "address": address,
       "dateOfBirth": dateOfBirth,
       "gender": gender,
     };
+  }
+
+  factory SignUpModel.fromJson(Map<String, dynamic> json) {
+    return SignUpModel(
+        firstName: json['firstName'],
+        lastName: json['lastName'],
+        email: json['email'],
+        password: json['password'],
+        phoneNumber: json['phoneNumber'],
+        countryCode: json['countryCode'],
+        country: json['country'],
+        city: json['city'],
+        jobTitle: json['jobTitle'],
+        companyName: json['companyName'],
+        school: json['school'],
+        startDate: json['startDate'],
+        location: json['location'],
+        address: json['address'],
+        dateOfBirth: json['dateOfBirth']);
   }
 }
