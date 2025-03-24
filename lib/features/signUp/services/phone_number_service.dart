ae8bacc0ab60549d56034a5a1112c345beb0532f
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-24 02:06:06 +0200

    Modification: Removed Redundant code

--- lib/features/signUp/services/phone_number_service.dart
+++ lib/features/signUp/services/phone_number_service.dart
@@ -1,21 +1,14 @@
 import 'package:link_up/features/signUp/model/phone_number_model.dart';
 
 class PhoneNumberService {
-  String? phoneNumber;
 
   Future<bool?> setPhoneNumber(PhoneNumberModel phoneNumber) async {
     try {
       await Future.delayed(Duration(seconds: 1));
-
-      this.phoneNumber = '+${phoneNumber.countryCode} ${phoneNumber.number}';
       return true;
     } catch (e) {
       return false;
     }
-  }
-
-  String? getPhoneNumber() {
-    return phoneNumber;
   }
 
   Future<bool> sendphonenumber() async {
