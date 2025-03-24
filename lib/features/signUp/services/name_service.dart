610fc7b641b57d2e01541c8f61f2c0ccd15a3399
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-24 02:05:27 +0200

    Modification: removed usless async function

--- lib/features/signUp/services/name_service.dart
+++ lib/features/signUp/services/name_service.dart
@@ -3,10 +3,8 @@ import 'package:link_up/features/signUp/model/name_model.dart';
 class NameService {
   String? name;
 
-  Future<bool?> setName(NameModel name) async {
+  bool? setName(NameModel name) {
     try {
-      await Future.delayed(Duration(seconds: 1));
-
       this.name = '${name.firstName!} ${name.lastName!}';
       return true;
     } catch (e) {
