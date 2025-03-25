1cfc6e8c0a2530d0b9395223c7ea73a493b1f70f
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-25 02:17:12 +0200

    Modification: Allowed base service to handle cookies

--- lib/core/services/base_service.dart
+++ lib/core/services/base_service.dart
@@ -10,35 +10,33 @@ import 'package:link_up/core/constants/endpoints.dart';
 
 // Add put/patch and delete methods
 class BaseService {
+  Map<String, String> headers = {};
+
   Future<Response> post(String endpoint, Map<String, dynamic> body) async {
     final token = await getToken();
     final uri = Uri.parse('${ExternalEndPoints.baseUrl}$endpoint');
+    headers['Content-Type'] = 'application/json';
+    headers['Authorization'] = 'Bearer $token';
     final response = await http
-        .post(
-          uri,
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer $token'
-          },
-          body: jsonEncode(body),
-        )
+        .post(uri, headers: headers, body: jsonEncode(body))
         .timeout(const Duration(seconds: 10));
+    updateCookie(response);
     return response;
   }
 
   Future<Response> put(String endpoint, Map<String, dynamic> body) async {
     final token = await getToken();
     final uri = Uri.parse('${ExternalEndPoints.baseUrl}$endpoint');
+    headers['Content-Type'] = 'application/json';
+    headers['Authorization'] = 'Bearer $token';
     final response = await http
         .put(
           uri,
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer $token'
-          },
+          headers: headers,
           body: jsonEncode(body),
         )
         .timeout(const Duration(seconds: 10));
+    updateCookie(response);
     return response;
   }
 
@@ -48,27 +46,40 @@ class BaseService {
     final uri = Uri.parse('${ExternalEndPoints.baseUrl}$endpoint').replace(
       queryParameters: queryParameters,
     );
-    final response = await http.get(
-      uri,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token'
-      },
-    ).timeout(const Duration(seconds: 10));
+    headers['Content-Type'] = 'application/json';
+    headers['Authorization'] = 'Bearer $token';
+    final response = await http
+        .get(
+          uri,
+          headers: headers,
+        )
+        .timeout(const Duration(seconds: 10));
+    updateCookie(response);
     return response;
   }
 
   Future<Response> delete(String endpoint) async {
     final token = await getToken();
     final uri = Uri.parse('${ExternalEndPoints.baseUrl}$endpoint');
-    final response = await http.delete(
-      uri,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token'
-      },
-    ).timeout(const Duration(seconds: 10));
+    headers['Content-Type'] = 'application/json';
+    headers['Authorization'] = 'Bearer $token';
+    final response = await http
+        .delete(
+          uri,
+          headers: headers,
+        )
+        .timeout(const Duration(seconds: 10));
+    updateCookie(response);
     return response;
+  }
+
+  void updateCookie(http.Response response) {
+    String? rawCookie = response.headers['set-cookie'];
+    if (rawCookie != null) {
+      int index = rawCookie.indexOf(';');
+      headers['cookie'] =
+          (index == -1) ? rawCookie : rawCookie.substring(0, index);
+    }
   }
 }
 
