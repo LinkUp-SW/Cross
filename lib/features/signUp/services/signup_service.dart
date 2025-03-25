71cc0c4e27c4cb74de83dc73da131a5fff1f7829
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-25 02:20:02 +0200

    Modifications: Removed redundancy

--- lib/features/signUp/services/signup_service.dart
+++ lib/features/signUp/services/signup_service.dart
@@ -6,7 +6,7 @@ import 'package:link_up/features/signUp/model/signup_global_model.dart';
 class SignUpService extends BaseService {
   Future<bool> sendDataToBackend(SignUpModel signUpData) async {
 
-    final response=await this.post("api/v1/user/signup/starter", signUpData.toJson());
+    final response=await post("api/v1/user/signup/starter", signUpData.toJson());
 
     if (response.statusCode == 200) {
       return true; // Successfully sent data
