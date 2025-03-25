d3a3506a44cf24f347381e7545549d4e286fc9af
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-03-25 02:18:11 +0200

    Bug fixes: fixed API handle

--- lib/features/logIn/services/login_service.dart
+++ lib/features/logIn/services/login_service.dart
@@ -8,10 +8,9 @@ import 'package:link_up/features/logIn/model/login_model.dart';
 
 class LogInService extends BaseService {
   Future<bool> logIn(LogInModel logInModel) async {
-    final response = await post("api/auth/login",
+    final response = await post("auth/login",
         {"email": logInModel.email, "password": logInModel.password});
 
-    print(jsonDecode(response.body));
     print(response.statusCode);
 
     if (response.statusCode == 200) {
