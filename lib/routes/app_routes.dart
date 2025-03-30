cd730e27ff2ff8de2c01160c98e608b431049ed6
amr21ss <amrsafwat23@hotmail.com>
2025-03-30 15:44:39 +0200

    added routes for sub pages onn edit intro page

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -6,12 +6,36 @@ import 'package:go_router/go_router.dart';
 import 'package:link_up/shared/dummy_page.dart';
 import 'package:link_up/shared/myhomepage.dart';
 import 'package:link_up/shared/widgets/bottom_navigation_bar.dart';
+import 'package:link_up/features/profile/view/view.dart';
+import 'package:link_up/features/profile/view/edit_intro.dart';
+import 'package:link_up/features/profile/view/edit_contact_info.dart';
+import 'package:link_up/features/profile/view/add_new_position.dart';
+import 'package:link_up/features/profile/view/add_new_education.dart';
+
 
 final goRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(initialLocation: '/', routes: <RouteBase>[
-    GoRoute(path: "/profile", builder: (context, state) => Container()),
+  return GoRouter(initialLocation: '/profile', routes: <RouteBase>[
+    GoRoute(path: "/profile", builder: (context, state) => const ProfilePage()),
     GoRoute(path: "/login", builder: (context, state) => Container()),
     GoRoute(path: "/signup", builder: (context, state) => Container()),
+    //Profile Page Routes
+    GoRoute(
+      path: "/edit_intro",
+      builder: (context, state) => const EditIntroPage(),
+    ),
+      GoRoute(
+      path: "/edit_contact_info",
+      builder: (context, state) => const EditContactInfo(),
+    ),
+      GoRoute(
+      path: "/add_new_position",
+      builder: (context, state) => const AddNewPosition(),
+    ),
+      GoRoute(
+      path: "/add_new_education",
+      builder: (context, state) => const AddNewEducation(),
+    ),
+
     StatefulShellRoute.indexedStack(
       builder: (context, state, navigationShell) => Scaffold(
         body: navigationShell, // The body displays the current screen
