8d0bd7bccf999cf0b8a56302ebe6bed01144f1da
Jumana Ahmed <jumana.elashry@gmail.com>
2025-04-21 05:32:16 +0200

    added mock data for testing

--- lib/features/chat/services/chat_service.dart
+++ lib/features/chat/services/chat_service.dart
@@ -2,6 +2,5 @@ import 'package:link_up/features/chat/model/chat_model.dart';
 
 abstract class ChatService {
   Future<List<Chat>> fetchChats();
-  Future<void> sendTypingStatus(int chatIndex, bool isTyping);
-  Stream<bool> getTypingStatus(int chatIndex);
+
 }
