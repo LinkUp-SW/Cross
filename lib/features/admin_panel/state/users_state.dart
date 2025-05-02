b2309732d426630939acdb45a2070d74ce0b5f5b
Youssef-Awad2004 <Youssef.Hassan04@eng-st.cu.edu.eg>
2025-05-03 00:59:26 +0300

    Add user management features including model, service, state, and UI components

---
+++ lib/features/admin_panel/state/users_state.dart
@@ -0,0 +1,23 @@
+import 'package:link_up/features/admin_panel/model/users_model.dart';
+
+sealed class UserState {
+  const UserState();
+}
+
+class UserInitial extends UserState {
+  const UserInitial();
+}
+
+class UserLoading extends UserState {
+  const UserLoading();
+}
+
+class UserLoaded extends UserState {
+  final List<UserModel> users;
+  const UserLoaded(this.users);
+}
+
+class UserError extends UserState {
+  final String message;
+  const UserError(this.message);
+}
